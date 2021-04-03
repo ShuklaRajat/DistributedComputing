21788e2e1c5d936d3d57c1a7fe5f53ba4200183f
Rajat Shukla <rajats.think@gmail.com>
2021-04-04 00:31:10 +0530

    Lai Yang Algorithm. 1. Able to calculate channel state for receiving processes.

--- LaiYangAlgorithm.py
+++ LaiYangAlgorithm.py
@@ -1,3 +1,5 @@
+from collections import OrderedDict
+
 numProcesses = 2
 processIdentifier = ['X', 'Y']
 
@@ -5,23 +7,23 @@ initialAmount = dict()
 initialAmount['X'] = 800
 initialAmount['Y'] = 200
 
-transaction_send = dict()
-transaction_receive = dict()
+transaction_send = OrderedDict()
+transaction_receive = OrderedDict()
 #transaction_send initiated by X of amount 20 between time period t1 and t2. The amount reaches process 'Y between time
 # period t2 and t3.
-transaction_send['X'] = [[20, 't1', 't2', 'Y', 't2', 't3']]
+transaction_send['X'] = [[20, 't1', 't2', 'Y']]
 transaction_receive['Y'] = [[20, 't2', 't3', 'X']]
 
-transaction_send['X'].append([30, 't2', 't3', 'Y', 't6', 't7'])
+transaction_send['X'].append([30, 't2', 't3', 'Y'])
 transaction_receive['Y'].append([30, 't6', 't7', 'X'])
 
-transaction_send['X'].append([10, 't3', 't4', 'Y', 't5', 't6'])
+transaction_send['X'].append([10, 't3', 't4', 'Y'])
 transaction_receive['Y'].append([10, 't5', 't6', 'X'])
 
-transaction_send['Y'] = [[30, 't4', 't5', 'X', 't6', 't7']]
+transaction_send['Y'] = [[30, 't4', 't5', 'X']]
 transaction_receive['X'] = [[30, 't6', 't7', 'Y']]
 
-transaction_send['Y'].append([20, 't7', 't8', 'X', 't8', 't9'])
+transaction_send['Y'].append([20, 't7', 't8', 'X'])
 transaction_receive['X'].append([20, 't8', 't9', 'Y'])
 
 print(transaction_send)
@@ -34,9 +36,11 @@ processColor = dict()
 # processColor['Y'] = ['Red', 't5', 't6']  #This needs to be computed programatically
 
 for i in range(len(transaction_send[snapshotAt[0]])):
-    # print(transaction_send['X'][i])
-    if transaction_send[snapshotAt[0]][i][1] == 't3':
-        processColor[transaction_send['X'][i][3]] = ['Red', transaction_send['X'][i][4], transaction_send['X'][i][5]]
+    if transaction_send[snapshotAt[0]][i][1] == snapshotAt[1]:
+        processColor[transaction_send[snapshotAt[0]][i][3]] \
+            = ['Red',
+                transaction_receive[transaction_send[snapshotAt[0]][i][3]][i][1],
+                transaction_receive[transaction_send[snapshotAt[0]][i][3]][i][2]]
 
 # print(processColor)
 
@@ -66,11 +70,12 @@ for processName in processIdentifier:
 # print(sum(channel_send['X']))
 
 for processName in processIdentifier:
-    if processName != snapshotAt[0]:    #For time being
+    if processName != snapshotAt[0]:
         for i in range(len(transaction_send[processName])):
 
             if int(transaction_send[processName][i][1].lstrip('t')) < int(processColor[processName][2].lstrip('t')) or \
-                (int(transaction_send[processName][i][1].lstrip('t')) == int(transaction_send[processName][i][2].lstrip('t'))
+                (int(transaction_send[processName][i][1].lstrip('t'))
+                 == int(transaction_send[processName][i][2].lstrip('t'))
                  == int(processColor[processName][2].lstrip('t'))):
 
                 if processName not in channel_send.keys():
@@ -83,8 +88,35 @@ print(channel_send)
 
 
 
+# for processName in processIdentifier:
+processName = snapshotAt[0]
+for i in range(len(transaction_receive[processName])):
+
+    if int(transaction_receive[processName][i][2].lstrip('t')) < snapshotAtTimeNumeric or \
+        (int(transaction_receive[processName][i][2].lstrip('t')) == int(transaction_receive[processName][i][2].lstrip('t'))
+         == snapshotAtTimeNumeric):
+
+        if processName not in channel_receive.keys():
+            channel_receive[processName] = []
+
+        channel_receive[processName].append(transaction_receive[processName][i][0])
 
 
+for processName in processIdentifier:
+    if processName != snapshotAt[0]:
+        for i in range(len(transaction_receive[processName])):
+            if int(transaction_receive[processName][i][2].lstrip('t')) < int(processColor[processName][2].lstrip('t')) or \
+                (int(transaction_receive[processName][i][1].lstrip('t'))
+                 == int(transaction_receive[processName][i][2].lstrip('t'))
+                 == int(processColor[processName][2].lstrip('t'))):
+
+                if processName not in channel_receive.keys():
+                    channel_receive[processName] = []
+
+                channel_receive[processName].append(transaction_receive[processName][i][0])
+
+
+print(channel_receive)
 
 
 
